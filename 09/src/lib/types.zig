9b9aabc9774eea8b1f3f361f9f15bebcbefcdea4
Sleantt <luca.13091997@gmail.com>
2025-12-10 16:39:02 +0100

    wip: 09

--- 09/src/lib/types.zig
+++ 09/src/lib/types.zig
@@ -2,40 +2,59 @@ const std = @import("std");
 pub const Vertex = struct {
     x: i64,
     y: i64,
-};
-
-pub const Edge = struct {
-    vertexes: [2]Vertex,
-};
-
-pub const Polygon = struct {
-    vertexEdges: std.AutoHashMap(Vertex, std.ArrayList(Edge)),
-    vertexes: []Vertex,
-    edges: []Edge,
-    pub fn init(allocator: std.mem.Allocator, vertexes: []Vertex) !Polygon {
-        const verts = try allocator.alloc(Vertex, vertexes.len);
-        const edges = try allocator.alloc(Edge, vertexes.len);
-        var vertexEdges = std.AutoHashMap(Vertex, std.ArrayList(Edge)).init(allocator);
-        @memcpy(verts, vertexes);
-        for (0..verts.len) |i| {
-            const j = (i + 1) % verts.len;
-            const current = verts[i];
-            const next = verts[j];
-            const edge = Edge{ .vertexes = .{ current, next } };
-            edges[i] = edge;
-
-            var currentEdges = try vertexEdges.getOrPutValue(
-                current,
-                std.ArrayList(Edge).empty,
-            );
-            var nextEdges = try vertexEdges.getOrPutValue(
-                next,
-                std.ArrayList(Edge).empty,
-            );
-            try currentEdges.value_ptr.append(allocator, edge);
-            try nextEdges.value_ptr.append(allocator, edge);
-        }
-        return Polygon{ .vertexEdges = vertexEdges, .vertexes = verts, .edges = edges };
+    pub fn equals(self: *const Vertex, other: Vertex) bool {
+        return self.x == other.x and self.y == other.y;
     }
 };
+fn signedArea(v1: Vertex, v2: Vertex, v3: Vertex) i64 {
+    return (v2.x - v1.x) * (v3.y - v1.y) - (v3.x - v1.x) * (v2.y - v1.y);
+    // negative if clockwise; twice the area of the triangle p1-p2-p3
+}
 
+fn ccw(v1: Vertex, v2: Vertex, v3: Vertex) i64 {
+    const a = signedArea(v1, v2, v3);
+    if (a < 0)
+        return -1;
+    if (a > 0)
+        return 1;
+    return 0;
+}
+
+fn isOnSegment(v: Vertex, from: Vertex, to: Vertex) bool {
+    return ccw(from, to, v) == 0 and
+        v.x >= @min(from.x, to.x) and
+        v.x <= @max(from.x, to.x) and
+        v.y >= @min(from.y, to.y) and
+        v.y <= @max(from.y, to.y);
+}
+fn intersect(s0from: Vertex, s0to: Vertex, s1from: Vertex, s1to: Vertex) bool {
+    return ((ccw(s0from, s0to, s1from) * ccw(s0from, s0to, s1to)) <= 0) and
+        ((ccw(s1from, s1to, s0from) * ccw(s1from, s1to, s0to)) <= 0);
+}
+
+pub const Polygon = struct {
+    allocator: std.mem.Allocator,
+    vertexes: []Vertex,
+    pub fn init(allocator: std.mem.Allocator, vertexes: []Vertex) !Polygon {
+        const verts = try allocator.alloc(Vertex, vertexes.len);
+        @memcpy(verts, vertexes);
+        return Polygon{ .allocator = allocator, .vertexes = verts };
+    }
+    pub fn area() i64 {}
+};
+
+pub fn polygonArea(vertices: []const Vertex) i64 {
+    if(vertices.len == 0) return 0;
+    var sum1: f64 = 0;
+    var sum2: f64 = 0;
+
+    for (0..vertices.len) |current| {
+        const next = (current + 1) % vertices.len;
+        sum1 += @floatFromInt(vertices[current].x * vertices[next].y);
+        sum2 += @floatFromInt(vertices[current].y * vertices[next].x);
+    }
+
+    sum1 += @floatFromInt(vertices[vertices.len-1].x * vertices[0].y);
+    sum2 += @floatFromInt(vertices[vertices.len-1].y * vertices[0].x);
+    return @intFromFloat(@abs(sum1 - sum2) / 2);
+}
--- 09/src/main.zig
+++ 09/src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const _09 = @import("_09");
-const sls = @import("lib/sweepline.zig");
 const types = @import("lib/types.zig");
 const Polygon = types.Polygon;
 const Vertex = types.Vertex;
@@ -26,17 +25,49 @@ fn readInput(allocator: std.mem.Allocator, filename: []const u8) !void {
         const y = try std.fmt.parseInt(i64, ptIt.next().?, 10);
         const newPoint = Vertex{ .x = x, .y = y };
         for (points.items) |point| {
-            const newArea = sls.area(newPoint, point);
+            const rect = rectFromAngles(point, newPoint);
+            const newArea = types.polygonArea(rect);
             maxArea = @max(maxArea, newArea);
         }
         try points.append(allocator, newPoint);
     }
-    std.debug.print("P1 : {any}\n", .{ maxArea});
+    std.debug.print("P1 : {any}\n", .{maxArea});
 
-    const p1 = try Polygon.init(allocator, points.items);
-    const p2 = try Polygon.init(allocator, points.items);
-    var sw = sls.SweepLine.init(allocator, p1, p2);
-    try sw.reset();
+    maxArea = 0;
+    const fullArea = types.polygonArea(points.items);
+    for (0..points.items.len) |i| {
+        const a = points.items[i];
+        for (i + 1..points.items.len) |j| {
+            const c = points.items[j];
+            const rect = rectFromAngles(a, c);
+            const rectArea = types.polygonArea(rect);
+            const p1Area = types.polygonArea(points.items[i + 1 .. j]);
+            var p2 = std.ArrayList(Vertex).empty;
+            for (points.items[j..]) |v| {
+                try p2.append(allocator, v);
+            }
+            for (points.items[0..i]) |v| {
+                try p2.append(allocator, v);
+            }
+            const p2Area = types.polygonArea(p2.items);
+            if (fullArea - rectArea == p1Area + p2Area) {
+                maxArea = @max(maxArea, rectArea);
+            }
+        }
+    }
+    std.debug.print("P2 : {any}\n", .{maxArea});
 }
 
+fn rectFromAngles(v1: Vertex, v2: Vertex) []const Vertex {
+    const maxX = @max(v1.x,v2.x);
+    const maxY = @max(v1.y,v2.y);
+    const minX = @min(v1.x,v2.x);
+    const minY = @min(v1.y,v2.y);
 
+    const a = Vertex{.x=minX, .y=minY};
+    const b = Vertex{.x=maxX, .y=minY};
+    const c = Vertex{.x=maxX, .y=maxY};
+    const d = Vertex{.x=minX, .y=maxY};
+    const res : [4]Vertex = .{a,b,c,d};
+    return &res;
+}
